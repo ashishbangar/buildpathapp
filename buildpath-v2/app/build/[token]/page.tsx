b80aebef94d83c9914053bb2b384e324cd78a0e0
ashishbangar <ashish.bangar.iiitm@gmail.com>
2026-06-15 03:58:49 +0530

    Delete buildpath-v2 directory

--- buildpath-v2/app/build/[token]/page.tsx
+++
@@ -1,8 +0,0 @@
-import Workspace from "./Workspace";
-
-export const dynamic = "force-dynamic";
-
-export default async function BuildPage({ params }: { params: Promise<{ token: string }> }) {
-  const { token } = await params;
-  return <Workspace token={token} />;
-}
--- buildpath-v2/next.config.ts
+++
@@ -1,1 +0,0 @@
-import type { NextConfig } from "next"; const c: NextConfig = {}; export default c;
